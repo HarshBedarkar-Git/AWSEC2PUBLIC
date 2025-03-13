8a9656a00ed03de6987ec7df66c2d82371417477
root <tomesh.verma@gmail.com>
2025-03-13 12:40:56 +0000

    first commmit from ec2

---
+++ MyFirstProgram.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class MyFirstProgram{
+public static void main(String args[]){
+System.out.println("Hi Java");
+int a;
+int b;
+System.out.println("Enter two number");
+Scanner sc = new Scanner(System.in);
+a= sc.nextInt();
+b=sc.nextInt();
+
+System.out.println("\n"+a+"+"+b+"="+(a+b));
+
+}
+}
