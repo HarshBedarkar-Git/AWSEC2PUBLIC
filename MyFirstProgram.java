af0c1f1851338a4780ffaf651373f1e7480a5b5b
HarshBedarkar-Git <151444133+HarshBedarkar-Git@users.noreply.github.com>
2025-03-22 18:34:38 +0530

    Revert "First pull request, source: Dev(Source) and Target: master(base)"

--- MyFirstProgram.java
+++ MyFirstProgram.java
@@ -10,8 +10,7 @@ Scanner sc = new Scanner(System.in);
 a= sc.nextInt();
 b=sc.nextInt();
 
-System.out.println("\n Sum of two numbers is "+a+"+"+b+"="+(a+b));
-System.out.println("\n Multiplication of two numbers is "+a+"x"+b+"="+(a*b));
-System.out.println("\n Subtraction of two mumber is "+a+"-"+b+"="+(a-b));
+System.out.println("\n"+a+"+"+b+"="+(a+b));
+
 }
 }
